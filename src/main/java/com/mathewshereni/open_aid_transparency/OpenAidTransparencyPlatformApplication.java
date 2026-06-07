d6962d3eb4d4268f5b80b07a1976a2973e1864c5
MATHEW-SHERENI <mashereni@gmail.com>
2026-06-07 13:22:45 -0500

    first commit to GitHub OpenAidTransparencyPlatformApplication

---
+++ src/main/java/com/mathewshereni/open_aid_transparency/OpenAidTransparencyPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.mathewshereni.open_aid_transparency;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OpenAidTransparencyPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OpenAidTransparencyPlatformApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/mathewshereni/open_aid_transparency/OpenAidTransparencyPlatformApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mathewshereni.open_aid_transparency;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OpenAidTransparencyPlatformApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
